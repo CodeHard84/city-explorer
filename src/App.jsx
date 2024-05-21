486d3f6e02b73ce4c4f35f3f53aabd8e178ac689
Jace <sockgnome@att.net>
2024-05-21 03:00:10 +0000

    feat: Add axios dependency and implement city search functionality
    This commit adds the axios dependency to the project and implements the functionality to search for a city using the entered city name. It also includes an input field and a button in the `App` component to facilitate the city exploration feature.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css'
+import axios from 'axios';
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  // Constants
+  const [location, setLocation] = useState({display_name: 'OKC'});
+  const [searchQuery, setSearchQuery] = useState('');
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  // Functions
+  async function getLocation() {
+    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;   
+    const response = await axios.get(API);
+    console.log(response);
+  }
+
+  function updateQuery(event) {
+    setSearchQuery(event.target.value);
+  }
+
   return (
-    <>
-      <h1>City-Explorer</h1>
-      <input type="text" placeholder="City Name" />
-      <button>Explore!</button>
-    </>
-  )
+    <div className="app-container">
+      <h1>City Explorer</h1>
+      <input id="citySearch" name="citySearch" type="text" placeholder="Enter city name" onChange={updateQuery}/>
+      <button id="exploreButton" name="exploreButton" onClick={getLocation}>Explore!</button>
+      <h2>City: {location.display_name}</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
