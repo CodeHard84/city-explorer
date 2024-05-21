9cc1aac7fcf3f0ce0db47597896c4a651a09c7ea
Jace <sockgnome@att.net>
2024-05-21 03:24:19 +0000

    feat: Refactor city name display in App component
    The commit refactors the `App` component to improve the display of the city name. It introduces a new function `prettyCityName` that removes the state and country from the city name, leaving only the city name itself. This change enhances the readability and clarity of the displayed city name.

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,15 @@ function App() {
     const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
     const response = await axios.get(API);
     setLocation(response.data[0]);
-    console.log(response);
   }
 
   function updateQuery(event) {
     setSearchQuery(event.target.value);
+  }
+
+  function prettyCityName(city) {
+    // Removes the state and country from the city name, leaving only the city name.
+    return city.split(',')[0];
   }
 
   return (
@@ -28,7 +32,7 @@ function App() {
         <button id="exploreButton" name="exploreButton" onClick={getLocation}>Explore!</button>
       </div>
       <div id="cityDetails">
-        <h2>City: {location.display_name}</h2>
+        <h2>City: {prettyCityName((location.display_name))}</h2>
         <h3>Latitude: {location.lat}</h3>
         <h3>Longitude: {location.lon}</h3>
       </div>
