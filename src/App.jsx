e5af9ed62cea96082266ad944d00df6b789ab245
Jace <sockgnome@att.net>
2024-05-21 10:53:02 +0000

    feat: Refactor city name display and improve city exploration UI in App component added map.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,18 @@ import './App.css';
 
 function App() {
   // Constants
-  const [location, setLocation] = useState({ display_name: '' }); // State to hold the location data
+  const [location, setLocation] =
+    useState({
+      display_name: 'Oklahoma City',
+      lat: '35.4729886',
+      lon: '-97.5170536',
+      icon: "https://locationiq.org/static/images/mapicons/poi_boundary_administrative.p.20.png",
+      license: "https://locationiq.com/attribution"
+    }); // State to hold the location data
   const [searchQuery, setSearchQuery] = useState(''); // State to hold the user's search query
   const API_KEY = import.meta.env.VITE_API_KEY; // API key for accessing the LocationIQ API
+  const ZOOM = 10; // Zoom level for the map
+  const MAP_API = `https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=${ZOOM}&size=450x450&format=png&maptype=streets&markers=icon:${location.icon}`;
 
   // Functions
   async function getLocation() {
@@ -16,6 +25,7 @@ function App() {
     const response = await axios.get(API);
     // Sets the location state with the first result from the API response
     setLocation(response.data[0]);
+    console.log(response.data[0]);
   }
 
   function updateQuery(event) {
@@ -28,29 +38,43 @@ function App() {
     return city.split(',')[0];
   }
 
+  // Is there a better way to write this so I don't have to check every key press?
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      getLocation();
+    }
+  }
+
   return (
     <div className="app-container">
       <h1>City Explorer</h1>
       <div id="uiButtons">
-        <input 
-          id="citySearch" 
-          name="citySearch" 
-          type="text" 
-          placeholder="Enter city name" 
+        <input
+          id="citySearch"
+          name="citySearch"
+          type="text"
+          placeholder="Enter city name"
           onChange={updateQuery} // Calls updateQuery function on input change
+          onKeyDown={handleKeyDown} // Check for enter button press
         />
-        <button 
-          id="exploreButton" 
-          name="exploreButton" 
+        <button
+          id="exploreButton"
+          name="exploreButton"
           onClick={getLocation} // Calls getLocation function on button click
         >
           Explore!
         </button>
       </div>
-      <div id="cityDetails">
-        <h2>City: {prettyCityName(location.display_name)}</h2> {/* Displays the city name up to the first comma */}
-        <h3>Latitude: {location.lat}</h3> {/* Displays the latitude of the location */}
-        <h3>Longitude: {location.lon}</h3> {/* Displays the longitude of the location */}
+      <div id="map-and-details">
+        <div id="cityDetails">
+          <h2>City: {prettyCityName(location.display_name)}</h2> {/* Displays the city name up to the first comma */}
+          <h3>Latitude: {location.lat}</h3> {/* Displays the latitude of the location */}
+          <h3>Longitude: {location.lon}</h3> {/* Displays the longitude of the location */}
+        </div>
+        <div id="map">
+          <img src={MAP_API} alt="Map" />
+          <p id='map-license'>Map of {location.display_name} by <a href={location.license} target="_blank">LocationIQ</a>.</p>
+        </div>
       </div>
     </div>
   );
