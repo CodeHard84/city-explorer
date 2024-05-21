e2c5bdaf0076d7de4396c7b6833a532ec99002b7
Jace <sockgnome@att.net>
2024-05-21 03:19:17 +0000

    feat: Improve city exploration UI in App component
    This commit enhances the city exploration UI in the `App` component by adding an input field and a button for users to enter a city name and explore it. The UI elements are wrapped in separate `<div>` containers for better organization. This change improves the user experience and makes the city exploration feature more intuitive.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import './App.css';
 
 function App() {
   // Constants
-  const [location, setLocation] = useState({display_name: ''});
+  const [location, setLocation] = useState({ display_name: '' });
   const [searchQuery, setSearchQuery] = useState('');
   const API_KEY = import.meta.env.VITE_API_KEY;
 
   // Functions
   async function getLocation() {
-    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;   
+    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
     const response = await axios.get(API);
     setLocation(response.data[0]);
     console.log(response);
@@ -23,11 +23,15 @@ function App() {
   return (
     <div className="app-container">
       <h1>City Explorer</h1>
-      <input id="citySearch" name="citySearch" type="text" placeholder="Enter city name" onChange={updateQuery}/>
-      <button id="exploreButton" name="exploreButton" onClick={getLocation}>Explore!</button>
-      <h2>City: {location.display_name}</h2>
-      <h3>Latitude: {location.lat}</h3>
-      <h3>Longitude: {location.lon}</h3>
+      <div id="uiButtons">
+        <input id="citySearch" name="citySearch" type="text" placeholder="Enter city name" onChange={updateQuery} />
+        <button id="exploreButton" name="exploreButton" onClick={getLocation}>Explore!</button>
+      </div>
+      <div id="cityDetails">
+        <h2>City: {location.display_name}</h2>
+        <h3>Latitude: {location.lat}</h3>
+        <h3>Longitude: {location.lon}</h3>
+      </div>
     </div>
   );
 }
