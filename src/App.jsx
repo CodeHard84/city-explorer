9486a535098adc7b7fca79e9c468f52d5ad2f58f
Jace <sockgnome@att.net>
2024-05-26 05:17:44 +0000

    Attempting a new build system.

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,10 @@ function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [hasSearched, setHasSearched] = useState(false);
-  const API_KEY = import.meta.env.VITE_API_KEY;
-  const USED_URL = import.meta.env.VITE_USED_URL;
+  // const API_KEY = import.meta.env.VITE_API_KEY;
+  // const USED_URL = import.meta.env.VITE_USED_URL;
+  const API_KEY = {{environment.VITE_API_KEY}};
+  const USED_URL = {{environment.VITE_USED_URL}};
   const ZOOM = 10;
 
   const getBoundingBoxPath = (boundingbox) => {
