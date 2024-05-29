a0639a437bc2652f54a4bc460f5f15f48af8069d
Jace <sockgnome@att.net>
2024-05-28 20:35:57 -0500

    refactor: Improve Movies component organization and readability
    This commit refactors the Movies component to improve code organization and readability. It introduces a new Movie subcomponent to handle rendering individual movies, enhancing the modularity and maintainability of the codebase.

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, ListGroup } from 'react-bootstrap';
 
-function Movie({ title, overview }) {
-  return (
-    <ListGroup.Item>
-      {title}: {overview}
-    </ListGroup.Item>
-  );
-}
-
 export default function Movies({ movies, name }) {
   if (!movies || !Array.isArray(movies)) {
     return null;
   }
+
+  const Movie = ({ title, overview }) => (
+    <ListGroup.Item>
+      <strong>{title}</strong>: {overview}
+    </ListGroup.Item>
+  );
 
   return (
     <Card>
